e8f4a8180b9d10025e2048550e5dcd1f449201d5
Jakaria Ahmed <jakaria88520@gmail.com>
2025-12-12 22:43:23 +0600

    resolve

--- src/hooks/useUserGetJob.jsx
+++ src/hooks/useUserGetJob.jsx
@@ -24,7 +24,7 @@ const useUserGetJob = () => {
       }
     };
     fetchAllJobs();
-  }, [dispatch]);
+  }, [dispatch, searchedQuery]);
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), 
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://swiftcareer-server.vercel.app',
+        changeOrigin: true,
+        secure: true
+      }
+    }
+  }
 })
