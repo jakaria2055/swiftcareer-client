0b4655548249810cecd248165cb278b08b620612
Jakaria Ahmed <jakaria88520@gmail.com>
2025-12-12 22:57:32 +0600

    resolve-2

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "/api/users"
-export const JOB_API_ENDPOINT = "/api/job"
-export const APPLICATION_API_ENDPOINT ="/api/application"
-export const COMPANY_API_ENDPOINT ="/api/company"
+export const USER_API_ENDPOINT = "https://swiftcareer-server.vercel.app/api/users"
+export const JOB_API_ENDPOINT = "https://swiftcareer-server.vercel.app/api/job"
+export const APPLICATION_API_ENDPOINT ="https://swiftcareer-server.vercel.app/api/application"
+export const COMPANY_API_ENDPOINT ="https://swiftcareer-server.vercel.app/api/company"
