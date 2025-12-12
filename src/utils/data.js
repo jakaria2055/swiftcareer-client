826ba8a117b0cc6c5bf95fac18d8314e690d54a0
Jakaria Ahmed <jakaria88520@gmail.com>
2025-12-12 23:05:55 +0600

    resolve-4

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "/api/users"
-export const JOB_API_ENDPOINT = "/api/job"
-export const APPLICATION_API_ENDPOINT ="/api/application"
-export const COMPANY_API_ENDPOINT ="/api/company"
+export const USER_API_ENDPOINT = `${import.meta.env.VITE_API_URL}/api/users`;
+export const JOB_API_ENDPOINT = `${import.meta.env.VITE_API_URL}/api/job`;
+export const APPLICATION_API_ENDPOINT = `${import.meta.env.VITE_API_URL}/api/application`;
+export const COMPANY_API_ENDPOINT = `${import.meta.env.VITE_API_URL}/api/company`;
