6c16f57cdc1cf8a28df58613062aa8acdd4c36ff
Jakaria Ahmed <jakaria88520@gmail.com>
2025-12-12 21:06:41 +0600

    vercel.json file added

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "http://localhost:5000/api/users"
-export const JOB_API_ENDPOINT = "http://localhost:5000/api/job"
-export const APPLICATION_API_ENDPOINT ="http://localhost:5000/api/application"
-export const COMPANY_API_ENDPOINT ="http://localhost:5000/api/company"
+export const USER_API_ENDPOINT = "https://swiftcareer-server.vercel.app/api/users"
+export const JOB_API_ENDPOINT = "https://swiftcareer-server.vercel.app/api/job"
+export const APPLICATION_API_ENDPOINT ="https://swiftcareer-server.vercel.app/api/application"
+export const COMPANY_API_ENDPOINT ="https://swiftcareer-server.vercel.app/api/company"
